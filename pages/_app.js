70168798c11b47fbaf2fb445ee2e7c64f7a16aaf
iamfarooqi <attaurrahmanfarooqi9@gmail.com>
2022-09-01 21:21:55 +0500

    changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { Web3Provider } from '@ethersproject/providers';
+import { Web3ReactProvider } from '@web3-react/core';
 import '../styles/globals.css'
 
+
+const getLibrary = async (provider) => {
+  return new Web3Provider(provider);
+};
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Component {...pageProps} />
+    </Web3ReactProvider>
+  </>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,114 @@
+import WalletConnectProvider from '@walletconnect/web3-provider';
+import { providers } from 'ethers';
 import Head from 'next/head'
 import Image from 'next/image'
+import Web3 from 'web3';
 import styles from '../styles/Home.module.css'
+import { ethers } from 'ethers'
+import { useEffect, useState } from 'react';
+import Web3Modal from 'web3modal';
+import CoinbaseWalletSDK from '@coinbase/wallet-sdk';
+import { useWeb3React } from '@web3-react/core';
+
+
+const providerOptions = {
+  coinbasewallet: {
+    package: CoinbaseWalletSDK,
+    options: {
+      appName: "Web3Modal Demo",
+      infuraId: "387142f2e9e74f3fa7c1c09a9ba226d6"
+    }
+  }
+}
+
 
 export default function Home() {
+  const { provider, account, chainId } = useWeb3React()
+  const [web3Provider, setWeb3Provider] = useState(null);
+  console.log("account", account);
+
+  var web3ModalProvider
+  async function connect() {
+    try {
+      let web3Modal = new Web3Modal({
+        cacheProvider: false,
+        providerOptions,
+      });
+      const web3ModalInstance = await web3Modal.connect();
+      web3ModalProvider = new ethers.providers.Web3Provider(web3ModalInstance);
+      console.log(web3ModalProvider);
+      console.log(web3ModalProvider.provider);
+      if (web3ModalProvider) {
+        setWeb3Provider(web3ModalProvider)
+      }
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  async function disconnect() {
+    try {
+      let web3Modal = new Web3Modal({
+        cacheProvider: false,
+        providerOptions,
+      });
+      const clear = web3Modal.clearCachedProvider();
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+
+  // //  Create WalletConnect Provider
+  // const provider = new WalletConnectProvider({
+  //   infuraId: "387142f2e9e74f3fa7c1c09a9ba226d6",
+  // });
+
+  // //  Enable session (triggers QR Code modal)
+  // let web3Provider;
+  // async function connectWallet(params) {
+  //   await provider.enable();
+
+  //   web3Provider = new providers.Web3Provider(provider);
+  //   provider.on("accountsChanged", (accounts) => {
+  //     console.log(accounts);
+  //     alert("your are connected successfully")
+  //   });
+  //   // const web3 = new Web3(provider);
+  //   // window.w3 = web3
+
+  //   // var accounts = await web3.eth.getAccounts(); // get all connected accounts
+  //   // // account = accounts[0]; // get the primary account
+  //   // console.log("accounts", accounts);
+  // }
+
+  // async function disconnect() {
+  //   await provider.disconnect()
+  //   alert("your are disconnected successfully")
+  // }
+  const NFTilityTokenAdd = "0xDA82dE74632F05fe11F418771B5a10449364eD5b";
+
+  const NFTilityTokenAbi = [{ "inputs": [], "stateMutability": "nonpayable", "type": "constructor" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "owner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "spender", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Approval", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "previousOwner", "type": "address" }, { "indexed": true, "internalType": "address", "name": "newOwner", "type": "address" }], "name": "OwnershipTransferred", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "address", "name": "account", "type": "address" }], "name": "Paused", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": true, "internalType": "address", "name": "from", "type": "address" }, { "indexed": true, "internalType": "address", "name": "to", "type": "address" }, { "indexed": false, "internalType": "uint256", "name": "value", "type": "uint256" }], "name": "Transfer", "type": "event" }, { "anonymous": false, "inputs": [{ "indexed": false, "internalType": "address", "name": "account", "type": "address" }], "name": "Unpaused", "type": "event" }, { "inputs": [{ "internalType": "address", "name": "owner", "type": "address" }, { "internalType": "address", "name": "spender", "type": "address" }], "name": "allowance", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "approve", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "account", "type": "address" }], "name": "balanceOf", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "burn", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "account", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "burnFrom", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "decimals", "outputs": [{ "internalType": "uint8", "name": "", "type": "uint8" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "subtractedValue", "type": "uint256" }], "name": "decreaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "spender", "type": "address" }, { "internalType": "uint256", "name": "addedValue", "type": "uint256" }], "name": "increaseAllowance", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "mint", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "name", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "owner", "outputs": [{ "internalType": "address", "name": "", "type": "address" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "pause", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "paused", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "renounceOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "symbol", "outputs": [{ "internalType": "string", "name": "", "type": "string" }], "stateMutability": "view", "type": "function" }, { "inputs": [], "name": "totalSupply", "outputs": [{ "internalType": "uint256", "name": "", "type": "uint256" }], "stateMutability": "view", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "transfer", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "from", "type": "address" }, { "internalType": "address", "name": "to", "type": "address" }, { "internalType": "uint256", "name": "amount", "type": "uint256" }], "name": "transferFrom", "outputs": [{ "internalType": "bool", "name": "", "type": "bool" }], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [{ "internalType": "address", "name": "newOwner", "type": "address" }], "name": "transferOwnership", "outputs": [], "stateMutability": "nonpayable", "type": "function" }, { "inputs": [], "name": "unpause", "outputs": [], "stateMutability": "nonpayable", "type": "function" }]
+
+
+  var NFTilityToken = new ethers.Contract(
+    NFTilityTokenAdd,
+    NFTilityTokenAbi,
+    provider
+  );
+
+  // console.log("NFTilityToken", NFTilityToken);
+
+  async function balanceOf() {
+    try {
+      var balanceOfAccount = await NFTilityToken.name();
+      // balanceOfAccount = balanceOfAccount.toString()
+      console.log("balanceOfAccount", balanceOfAccount);
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -13,43 +119,25 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Wallets
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        {/* <div className={styles.grid}>
+          <button onClick={connectWallet}>ConnectWallet</button>
+          <button onClick={disconnect}>Disconnect</button>
+        </div> */}
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {web3Provider == null ?
+          <>
+            <button onClick={connect}>Connect</button>
+          </>
+          :
+          <>
+            <p>Connected</p>
+            <p>Address: {web3Provider.provider.selectedAddress}</p>
+            <button onClick={disconnect}>Disconnect</button>
+          </>}
+        <button onClick={balanceOf}>get Balance</button>
       </main>
 
       <footer className={styles.footer}>
